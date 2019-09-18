1afab14b1ec0d34a4d1aa6be2ecb9655c662fa35
Vilmos Nebehaj <v.nebehaj@gmail.com>
2019-09-18 14:45:50 -0700

    Remove YCM

--- provision.sh
+++ provision.sh
@@ -6,4 +6,3 @@ su -c "pip install ansible markupsafe"
 ansible-playbook -K -c local -i localhost, base-packages.yml
 ansible-playbook -K -c local -i localhost, workstation.yml
 ansible-playbook -c local -i localhost, dotfiles.yml
-ansible-playbook -c local -i localhost, ycm.yml
