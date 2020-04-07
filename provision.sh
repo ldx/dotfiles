4aaac3cc3f7e0cab0aebd2d3c618dbd4c79eec6f
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-04-07 13:27:20 -0700

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+#
+# Run this without sudo to ensure dotfiles are installed for the right user:
+#     $ ./provision.sh
+#
 
+# Required packages.
 su -c "apt-get install python-dev python-pip"
-su -c "pip install ansible markupsafe"
+su -c "pip install ansible markupsafe rsync"
 
 ansible-playbook -K -c local -i localhost, base-packages.yml
 ansible-playbook -K -c local -i localhost, workstation.yml
 ansible-playbook -c local -i localhost, dotfiles.yml
+
+# Install latest Firefox.
+sudo apt-get remove firefox-esr || true
+curl -LO "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US" > FirefoxSetup.tar.bz2
+tar xvf FirefoxSetup.tar.bz2
+sudo mv firefox /opt/
+sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+rm FirefoxSetup.tar.bz2
