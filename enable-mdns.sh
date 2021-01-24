639278b35619474ef3e1197efbfab6c6418671ce
Vilmos Nebehaj <v.nebehaj@gmail.com>
2021-01-24 15:55:52 -0800

    Add enable-mdns.sh

---
+++ enable-mdns.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+WLAN_IF=wlp0s20f3
+
+sudo systemd-resolve --set-mdns=yes --interface=$WLAN_IF
+sudo systemd-resolve --status $WLAN_IF
+sudo systemctl restart systems-resolved 
