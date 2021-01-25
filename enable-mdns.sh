6ea0838cd2b2fe95f7379f34815f86e0c411a6e0
Vilmos Nebehaj <v.nebehaj@gmail.com>
2021-01-25 13:39:34 -0800

    Fix typo

--- enable-mdns.sh
+++ enable-mdns.sh
@@ -4,4 +4,4 @@ WLAN_IF=wlp0s20f3
 
 sudo systemd-resolve --set-mdns=yes --interface=$WLAN_IF
 sudo systemd-resolve --status $WLAN_IF
-sudo systemctl restart systems-resolved 
+sudo systemctl restart systemd-resolved 
