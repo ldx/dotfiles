b08c84448cad9b4425d9d16e3334e959636faac2
Vilmos Nebehaj <v.nebehaj@gmail.com>
2025-05-13 14:47:53 -0700

    Map C-d to C-k

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
 vim.keymap.del({ "n", "i", "v" }, "<A-j>")
 vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+
+map({ "i", "v" }, "<C-d>", "<C-k>")
