2dbc9f2ff49ed4167bd84525081932745fafc00e
Vilmos Nebehaj <v.nebehaj@gmail.com>
2025-06-14 18:54:47 -0700

    Add vtsls to lazyvim

--- dotfiles/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,17 +1,29 @@
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		servers = { eslint = {} },
-		setup = {
-			eslint = function()
-				require("lazyvim.util").lsp.on_attach(function(client)
-					if client.name == "eslint" then
-						client.server_capabilities.documentFormattingProvider = true
-					elseif client.name == "tsserver" then
-						client.server_capabilities.documentFormattingProvider = false
-					end
-				end)
-			end,
-		},
-	},
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      eslint = {},
+      vtsls = {
+        settings = {
+          experimental = {
+            completion = {
+              enableServerSideFuzzyMatch = true,
+              entriesLimit = 200,
+            },
+          },
+        },
+      },
+    },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
+  },
 }
